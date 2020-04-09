22ddc3d007bfd3152478811a031f62491bce7523
ch33s322 <61012984+ch33s322@users.noreply.github.com>
2020-04-09 09:38:57 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Discord = require("discord.js"); //library for interacting with discord
 const YTDL = require("YTDL-core"); //library for interacting with yt and playing music in vc
 const fs = require("fs"); //library for file system in node js
 
-const TOKEN = "no token 4 u"; //bot token 
+const TOKEN = "NTQ2MTUxMTgxMDU2MDgxOTIw.Xl2RMg.9ZX0am87Q0V5txLQkPysUdWeVHU"; //bot token 
 
 const PREFIX = "!"; //list of prefixes that get the bot's attention
 const PREFIX2 = "&";
@@ -25,7 +25,7 @@ function play(connection, message) {
 
     server.dispatcher.on("end", function() {
             if (server.queue[0]) play(connection, message);
-            else connection.dissconnect();
+            else message.guild.voiceConnection.dissconnect();
         }
 
     )
@@ -43,28 +43,21 @@ var fortunes = [
         "naw",
         "GeT oUt Of My RoOm I'm PlAyInG mInEcRaFt!!!!111!!!!"
     ]
-    //d 20 command
-var d20 = [
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-        "9",
-        "10",
-        "11",
-        "12",
-        "13",
-        "14",
-        "15",
-        "16",
-        "17",
-        "18",
-        "19",
-        "20"
+    //meme links
+var meme_links = [
+        "https://i.imgur.com/1OfmEIq.png",
+        "https://i.imgur.com/D5tW1m0.jpg",
+        "https://i.imgur.com/rzGLR47.png",
+        "https://i.imgur.com/cCyYkFO.png",
+        "https://i.imgur.com/vTQCJmD.png",
+        "https://i.imgur.com/VFv0IcJ.png",
+        "https://i.imgur.com/nvHuSSL.png",
+        "https://i.imgur.com/05eHLDe.png",
+        "https://i.imgur.com/LcseHpR.jpg",
+        "https://i.imgur.com/VGT7Qr5.jpg",
+        "https://i.imgur.com/rKjNi1v.png",
+        "https://i.imgur.com/eATCFWD.jpg",
+        "https://i.imgur.com/6jSvPo4.png"
     ]
     //random descriptions for profile command
 var descriptors = [
@@ -93,10 +86,38 @@ var descriptors = [
         "lonely boi"
     ]
     //on sartup let me know that the bot is online
-Bot.on("ready", function() {
+
+Bot.on('ready', () => {
     console.log("ready");
+    Bot.user.setStatus('available')
+    Bot.user.setPresence({
+        game: {
+            name: 'freakin jams',
+            type: "LISTENING",
+            url: ""
+        }
+    });
+});
+Bot.on("guildMemberAdd", function(member) {
+    member.guild.channels.find("name", "general").sendMessage(member.toString() + " joined, freakin degenerate");
+
+    member.addRole(member.guild.roles.find("name", "AnimeComunists"));
 });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+//main commands
 Bot.on("message", function(message) {
     if (message.author.equals(Bot.user)) return; //dont let bots msg bots
 
@@ -115,42 +136,77 @@ Bot.on("message", function(message) {
             break;
         case "list":
             var embed = new Discord.RichEmbed()
-                .addField("List of Commands", "!ping - pong\n!8ball - tells your fortune\n!profile - bassically either roasts you or gives you a half compliment\n!play - The ultimate weapon of mass destruction\n!stop - doesnt work( i wont shut up when you play music)\n!skip - doesnt work(nats 10 hour of poopty scoop is goin all the way)")
+                .addField("List of Commands", "!ping - pong\n!8ball - tells your fortune\n!profile - bassically either roasts you or gives you a half compliment\n!play - The ultimate weapon of mass destruction\n!stop - doesnt work( i wont shut up when you play music)\n!skip - doesnt work(nats 10 hour of poopty scoop is goin all the way)\n!meme - gives you a sick meme from a repository of cowbowser related content")
                 .setColor(0x0fa567)
             message.channel.sendEmbed(embed);
+            break;
+        case "meme":
+            var MEME = new Discord.RichEmbed()
+                .setColor(0x0fa567)
+                .setTitle('More cowbower content here')
+                .setURL('https://www.reddit.com/r/CowBowser/')
+                .setAuthor('~OwO~', 'https://i.imgur.com/v4Gz4b9.png', 'https://www.reddit.com/r/CowBowser/')
+                .setDescription('The meme that you have called upon:')
+                .setThumbnail('https://i.imgur.com/RJu2v72.png')
+                .setImage((meme_links[Math.floor(Math.random() * meme_links.length)]))
+                .setTimestamp()
+                .setFooter('I take my leave', 'https://i.imgur.com/v4Gz4b9.png')
+            message.channel.sendEmbed(MEME);
             break;
 
         case "profile":
             //either get specific user or assign a random profile
-            switch (message.author.id) {
-                case 364133127238582273: //ch33s3
-                    var prof = new Discord.RichEmbed()
-                        .addField(message.author.username.toString(), "The one resposible for me, shoulda worn that condom!")
+            switch (message.author.username.toString()) {
+                case 'ch33s3': //ch33s3
+                    var ch33s3 = new Discord.RichEmbed()
+                        .addField(message.author.username.toString(), "They call him bot man...")
+                        .addField("about:", "the one resposible for coding me and all my functions")
                         .setColor(0x0fa655)
                         .setThumbnail(message.author.avatarURL)
-                    message.channel.sendEmbed(prof);
+                    message.channel.sendEmbed(ch33s3);
 
                     break;
+                case 'Frogdunker': //nat
+                    var ch33s3 = new Discord.RichEmbed()
+                        .addField(message.author.username.toString(), "Cowbowsers #1 fan!")
+                        .setColor(0x0fa655)
+                        .setThumbnail(message.author.avatarURL)
+                    message.channel.sendEmbed(ch33s3);
+                    break;
                 case 163108529689853953: // luke
-                    var prof = new Discord.RichEmbed()
+                    var luke = new Discord.RichEmbed()
                         .addField(message.author.username.toString(), "Designated commer via: mark")
                         .setColor(0x0fa655)
                         .setThumbnail(message.author.avatarURL)
-                    message.channel.sendEmbed(prof);
+                    message.channel.sendEmbed(luke);
                     break;
                 case 405896044870762507: // aiden
-                    var prof = new Discord.RichEmbed()
+                    var aids = new Discord.RichEmbed()
                         .addField(message.author.username.toString(), "warlock boi")
                         .setColor(0x0fa655)
                         .setThumbnail(message.author.avatarURL)
-                    message.channel.sendEmbed(prof);
+                    message.channel.sendEmbed(aids);
                     break;
                 case 117081368919408641: // mark
-                    var prof = new Discord.RichEmbed()
+                    var merk = new Discord.RichEmbed()
                         .addField(message.author.username.toString(), "certified coomer")
                         .setColor(0x0fa655)
                         .setThumbnail(message.author.avatarURL)
-                    message.channel.sendEmbed(prof);
+                    message.channel.sendEmbed(merk);
+                    break;
+                case 'Amphitrite': // julia
+                    var merk = new Discord.RichEmbed()
+                        .addField(message.author.username.toString(), "wants to be known as 'lonely boi'")
+                        .setColor(0x0fa655)
+                        .setThumbnail(message.author.avatarURL)
+                    message.channel.sendEmbed(merk);
+                    break;
+                case 'Octoeyepie': // rachel
+                    var rachel = new Discord.RichEmbed()
+                        .addField(message.author.username.toString(), "short:  4'3")
+                        .setColor(0x0fa655)
+                        .setThumbnail(message.author.avatarURL)
+                    message.channel.sendEmbed(rachel);
                     break;
                     //random case if not recignised
                 default:
@@ -161,6 +217,10 @@ Bot.on("message", function(message) {
                     message.channel.sendEmbed(prof);
                     break;
             }
+            break;
+        case "roleme":
+            var role = message.guild.roles.find("name", "Froggychair");
+            message.member.addRole(role);
             break;
         case "play":
             if (!args[1]) {
@@ -186,16 +246,31 @@ Bot.on("message", function(message) {
             //doesnt work needs fixing
         case "skip":
             var server = servers[message.guild.id];
-            if (server.dispacher) server.dispacher.end();
-            break;
+            if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
+                play(connection, message);
+            });
             //doesnt work needs fixing
         case "stop":
             var server = servers[message.guild.id];
             if (message.guild.voiceConnection) {
-                message.guild.voiceConnection.disconnect;
+                for (var i = server.queue.length - 1; i >= 0; i--) {
+                    server.queue.splice(i, 1);
+                }
+                server.dispatcher.end();
+                console.log("[" + new Date().toLocaleString() + "] Stopped the queue.");
             }
             break;
 
+        case "d":
+            var total = 1;
+            for (x = 0; x < args[2]; x++) {
+                total += Math.floor(Math.random() * Math.floor(args[1]));
+            }
+            message.channel.sendMessage(total);
+            break;
+        case "args":
+            message.channel.sendMessage(args);
+            break;
         default:
             message.channel.sendMessage("Invalid comand");
 
@@ -242,7 +317,7 @@ Bot.on("message", function(message) {
         break;
     }
 });
-
+//write anime file
 Bot.on("message", function(message) {
     if (message.author.equals(Bot.user)) return;
     if (!message.content.startsWith(PREFIX2)) return;
@@ -259,7 +334,7 @@ Bot.on("message", function(message) {
 });
 
 
-
+//read anime file
 Bot.on("message", function(message) {
     if (message.author.equals(Bot.user)) return;
     if (!message.content.startsWith(ANIMEFIX)) return;
